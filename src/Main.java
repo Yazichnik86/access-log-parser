7df76260fed3e88fb80688a3e26a77a8fa297eed
Yazichnik86 <a_s_gorbunov@list.ru>
2025-02-11 14:30:58 +0300

    Программа расчета математических действий над двумя числами. И выводом значений на экран.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма двух чисел:");
+        int summa=firstNumber+secondNumber;
+        System.out.println(summa);
+        System.out.println("Разность двух чисел:");
+        int raznost =firstNumber-secondNumber;
+        System.out.println(raznost);
+        System.out.println("Произведение двух чисел:");
+        int proizvedenie=firstNumber*secondNumber;
+        System.out.println(proizvedenie);
+        System.out.println("Частное двух чисел:");
+        double chastnoe=(double) firstNumber/secondNumber;
+        System.out.println(chastnoe);
     }
 }
