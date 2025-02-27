a8d2bb203cc858a245d9fc4e75b05dc856cb9d0a
Yazichnik86 <a_s_gorbunov@list.ru>
2025-02-27 19:52:12 +0300

    Программа проверки наличия файла по указанному адресу.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма двух чисел:");
-        int summa=firstNumber+secondNumber;
-        System.out.println(summa);
-        System.out.println("Разность двух чисел:");
-        int raznost =firstNumber-secondNumber;
-        System.out.println(raznost);
-        System.out.println("Произведение двух чисел:");
-        int proizvedenie=firstNumber*secondNumber;
-        System.out.println(proizvedenie);
-        System.out.println("Частное двух чисел:");
-        double chastnoe=(double) firstNumber/secondNumber;
-        System.out.println(chastnoe);
+    public static void main(String[] args) {
+
+        int N = 1;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if ((!fileExists) || (isDirectory)) {
+                System.out.println("Файл не существует или указанный путь является путём к папке");
+                continue;
+            }
+            System.out.println("Путь к файлу указан верно. Кол-во верно указанных путей к файлу: " + N);
+            N++;
+        }
     }
 }
